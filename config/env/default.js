f014ca286cc4a474249ad7d59daa1f3ab77b61bf
Andrew Throener <athroener@gmail.com>
2015-08-19 11:02:29 -0500

    Initial commit

---
+++ config/env/default.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  app: {
+    title: 'MEAN.JS',
+    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
+  },
+  port: process.env.PORT || 3000,
+  templateEngine: 'swig',
+  // Session details
+  // session expiration is set by default to 24 hours
+  sessionExpiration: 24 * (60 * 60 * 1000),
+  // sessionSecret should be changed for security measures and concerns
+  sessionSecret: 'MEAN',
+  // sessionKey is set to the generic sessionId key used by PHP applications
+  // for obsecurity reasons
+  sessionKey: 'sessionId',
+  sessionCollection: 'sessions',
+  logo: 'modules/core/img/brand/logo.png',
+  favicon: 'modules/core/img/brand/favicon.ico'
+};
